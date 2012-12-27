64659e156dcdec0579a2308843d111164b41132d
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-12-27 16:01:38 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoBackbone::Application.config.secret_token = 'bb528c82077f8ce6cc0027b85ea2af2969060f10c696af29240855e172a62c4b8df135747b00b214da5a7a349bb171750c3c21fa2036a59d25218749aa611115'
